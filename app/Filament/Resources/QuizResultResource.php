1eb92ce4c883ce82b5658cfe98b069dbe9ba258b
Anas Talal <anasctalal@gmail.com>
2024-07-12 22:52:21 +0300

    uo

---
+++ app/Filament/Resources/QuizResultResource.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\QuizResultResource\Pages;
+use App\Filament\Resources\QuizResultResource\RelationManagers;
+use App\Models\QuizResult;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class QuizResultResource extends Resource
+{
+    protected static ?string $model = QuizResult::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function getModelLabel(): string
+    {
+        return 'تحليل الاهتمامات';
+    }
+    public static function getPluralModelLabel(): string
+    {
+        return __('تحليل الاهتمامات');
+    }
+
+    public static function canCreate(): bool
+    {
+        return false;
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('user_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('pattern_id')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('user.name')->label('User Name')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('user.email') ->label('email')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('pattern.name')->label('pattern')
+                    ->sortable(),
+                Tables\Columns\ColorColumn::make('pattern.color')->label('color')
+                    ->sortable()
+                    ,
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable(),   
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                // Tables\Actions\ViewAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListQuizResults::route('/'),
+            // 'create' => Pages\CreateQuizResult::route('/create'),
+            // 'edit' => Pages\EditQuizResult::route('/{record}/edit'),
+        ];
+    }
+}
